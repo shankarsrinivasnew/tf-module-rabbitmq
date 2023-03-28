31833db22c7dd452f1e1991f2e0fba8b8c229b10
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-29 02:24:00 +0530

    gdfh

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ resource "aws_spot_instance_request" "rabbitmqr" {
   wait_for_fulfillment   = true
   vpc_security_group_ids = [aws_security_group.sgr.id]
   iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+    component = var.component
+    env       = var.env
+  }))
 
 
   tags = merge(
@@ -17,9 +21,9 @@ resource "aws_spot_instance_request" "rabbitmqr" {
 
 resource "aws_ec2_tag" "name-tag" {
   resource_id = aws_spot_instance_request.rabbitmqr.spot_instance_id
-  key = "Name"
-  value = "rabbitmq-${var.env}"
-  
+  key         = "Name"
+  value       = "rabbitmq-${var.env}"
+
 }
 resource "aws_security_group" "sgr" {
   name        = "rabbitmq-${var.env}-sg"
